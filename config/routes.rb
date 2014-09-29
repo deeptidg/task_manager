e72bd6fe09d1e6765bddb8226cdd88ee4e42acc2
deeptidg <deeptig@Maheshs-MacBook-2.local>
2014-09-28 23:28:14 -0400

    cleaned up routes. Removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 TaskManager::Application.routes.draw do
-    get "logout" => "sessions#destroy", :as => "logout"
+  get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
   resources :users
   resources :sessions
   resources :tasks
-  get "secret" => "home#secret", :as => "secret"
   root :to => "home#index"
 
 end
